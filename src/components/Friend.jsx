6481cc50519f75a746cd8f366e138bcad5401aa0
Digvijay Sethi <59555227+justxdv@users.noreply.github.com>
2023-04-05 23:18:33 +0530

    Update Friend.jsx

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -24,7 +24,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${currentUser._id}/${friendId}`,
+      `https://socialbee-drs.onrender.com/users/${currentUser._id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
